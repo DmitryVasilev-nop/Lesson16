6b9b00141e1cd00bae8983679ba281c60edcb27e
codeclass-pc-4 <dimonyatina@gmail.com>
2024-12-26 18:18:28 +0300

    lesson 16

--- triple_operator_01.py
+++ triple_operator_01.py
@@ -2,6 +2,7 @@ import random
 numbers = []
 for i in range(100):
     numbers.append(random.randint(0, 50))
+
 result = None
 result = 'Есть китайское несчастливое число' if 4 in numbers else result == 'Все нормально'
 print('Результат:\n', result)
